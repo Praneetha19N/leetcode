9873d26c8919077041ba48cc6ce1782abe29f4a7
Praneetha19N <nllspraneetha@gmail.com>
2026-06-16 06:42:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (65.21%)

---
+++ 2048-build-array-from-permutation/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        l=[]
+        for i in nums:
+            l.append(nums[i])
+        return l
+        
+
+        
+        
